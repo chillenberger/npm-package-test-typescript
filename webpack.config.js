55659528bd2b30ad8406843de1960e3dcae42062
Dan <dillenbe@gmail.com>
2022-08-12 13:11:02 -0600

    add storybook
    added storybook to make development easier for programmers.

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: [/node_modules/, /webpack.config.js/]
+                exclude: [/node_modules/]
             },
             {
                 test: /\.s[ac]ss$/i,
