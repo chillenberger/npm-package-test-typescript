b09ee04ef87d06255638fe97d8063c166cfe380c
Dan <dillenbe@gmail.com>
2022-08-19 08:04:24 -0600

    test(babe, jest.config, pakcage.json, Auth_Button.test, SkeletonTable.test) added testing to package

--- src/components/tables/SkeletonTable/SkeletonTable.test.tsx
+++ src/components/tables/SkeletonTable/SkeletonTable.test.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-import SkeletonTable from "./SkeletonTable";
+import SkeletonTable from './SkeletonTable';
 
-describe("SkeletonTable", () => {
-    test("renders the SkeletonTable component", () => {
-        render(<div data-testid="table"><SkeletonTable/></div>);
-    })
-})
+describe('SkeletonTable', () => {
+  test('renders the SkeletonTable component', () => {
+    render(
+      <div data-testid="table">
+        <SkeletonTable />
+      </div>,
+    );
+  });
+});
